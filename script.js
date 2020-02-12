b4d7559b17e6ce86d93314faad8a79aaae236595
Jack <jack.w141@outlook.com>
2020-02-12 14:50:09 +0000

    implement total fix for items being deleted

--- script.js
+++ script.js
@@ -15,9 +15,23 @@ let database = firebase.database();
 let form = document.querySelector('.form');
 let list = document.querySelector('.list');
 let runTotal = document.querySelector('.title-lead');
+let subTotal = 0;
 let items = [];
 
-document.body.addEventListener('load', readuserData());
+document.body.addEventListener('load', init());
+
+function init() {
+	readuserData();
+	priceCalc();
+}
+
+function priceCalc() {
+	console.log('price calc Called');
+	items.forEach((item) => {
+		subTotal = subTotal += parseInt(item.price);
+		runTotal.innerText = `£${String(subTotal)}`;
+	});
+}
 
 function writeUserData(item, price, id) {
 	console.log('write User Data Called');
@@ -33,63 +47,22 @@ function readuserData() {
 	let itemList = [];
 	const listOfItems = firebase.database().ref('items/');
 	listOfItems.once('value').then(function(snapshot) {
+		subTotal = 0;
 		snapshot.forEach(function(item) {
 			console.log('pushed');
 			console.log(items);
 			displayData(item.val());
+			runTotal.innerText = `Total - £${(subTotal += parseInt(item.val().price))}`;
 		});
 	});
+	return listOfItems;
 }
-
-// function updateData() {
-// 	let itemList = [];
-// 	const listOfItems = firebase.database().ref('items/');
-// 	listOfItems.once('value').then(function(snapshot) {
-// 		snapshot.forEach(function(item) {
-// 			items.push(item.val());
-// 			console.log('pushed');
-// 			console.log(items);
-// 		});
-// 	});
-// }
 
 function displayData(item) {
 	console.log('Display Data Called');
 	const html = `<li class="list-item" data-id="${item.id}">${item.item} <span class="price">£${item.price}</span><span class="delete-btn">X</span></li>`;
 	list.innerHTML += html;
-
-	priceCalc();
 }
-
-function priceCalc() {
-	console.log('price calc Called');
-	let total = 0;
-
-	items.forEach((item) => {
-		total += parseInt(item.price);
-	});
-
-	runTotal.innerText = `Total - £${total}`;
-
-	return total;
-}
-
-// function addPost(item, price) {
-// 	console.log('AddPost Called');
-//
-// 	writeUserData(item, price, id);
-// 	const html = `<li class="list-item" data-id=${id}">${item} <span class="price">£${price}</span><span class="delete-btn">X</span></li>`;
-// 	list.innerHTML += html;
-// 	items.push({
-// 		name: item,
-// 		price: price,
-// 		id: id
-// 	});
-
-// 	form.reset();
-// 	priceCalc();
-// 	console.log(items);
-// }
 
 function deletePost(id) {
 	console.log('delete Post Called');
@@ -103,22 +76,21 @@ form.addEventListener('submit', (e) => {
 	//    console.log(e.target.name.value);
 	// addPost(e.target.name.value.trim(), e.target.price.value);
 	writeUserData(e.target.name.value.trim(), e.target.price.value, id);
+	list.innerHTML = '';
 	readuserData();
+	form.reset();
 });
 
 list.addEventListener('click', (e) => {
 	if (e.target.classList.contains('delete-btn')) {
-		console.log('Deleted');
 		e.target.parentNode.parentNode.removeChild(e.target.parentNode);
 		let id = e.target.parentElement.getAttribute('data-id');
-		items = items.filter(function(item) {
-			return item != id ? item : null;
+		let deletedItem = database.ref('items/' + id);
+		deletedItem.on('value', function(snapshot) {
+			runTotal.innerText = `Total - £${(subTotal -= parseInt(snapshot.val().price))}`;
+			deletePost(id);
 		});
-		console.log(id);
 		// readuserData();
-		deletePost(id);
 		console.log('deleted from Firestore');
-		console.log(items);
-		priceCalc();
 	}
 });
